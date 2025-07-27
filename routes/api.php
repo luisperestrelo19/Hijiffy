7a7d2d9e48b6fd59cc64353cb53fc48dccec4dbf
Luis <luis.perestrelo@acin.pt>
2025-07-27 12:21:12 +0100

    Remove unused user route and clean up availability routes in API

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,13 @@ declare(strict_types=1);
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AvailablityController;
 use App\Http\Controllers\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout']);
-
+    Route::post('/availabilities', [AvailablityController::class, 'store']);
+    Route::get('/availabilities', [AvailablityController::class, 'index']);
 });
-
-Route::post('/availabilities', [AvailablityController::class, 'store']);
-Route::get('/availabilities', [AvailablityController::class, 'index']);
